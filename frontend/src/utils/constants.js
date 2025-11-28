732748cc070b66e5e5a1ebcd41227d83bd7b641b
Suvrat <suvratmittal007@gmail.com>
2025-11-28 21:40:28 +0530

    chore: added cron job and vercel workflow

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = 'http://localhost:8000';
-export const BASE_URL = 'https://portfolio-new-cn8b.onrender.com';
+// export const BASE_URL = 'https://portfolio-new-cn8b.onrender.com';
+export const BASE_URL = ' ';
